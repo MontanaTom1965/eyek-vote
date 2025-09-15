2b37c927d808f7e721f3ff22080c8b619bdc848d
MontanaTom1965 <165323793+MontanaTom1965@users.noreply.github.com>
2025-09-15 12:25:04 -0600

    Update get_state.php

--- assets/get_state.php
+++ assets/get_state.php
@@ -3,23 +3,29 @@ header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-store');
 
 $DATA = __DIR__ . '/state.json';
+
 if (!file_exists($DATA)) {
-  // Bootstrap a fresh state
   $state = [
     "serverUpdatedAt" => time(),
-    "event" => ["venue"=>"","venuePublic"=>"","date"=>date('Y-m-d'),"kj"=>"","pin"=>"","locked"=>false],
+    "event" => [
+      "venue" => "",
+      "venuePublic" => "",
+      "date" => date('Y-m-d'),
+      "kj" => "",
+      "pin" => "",
+      "locked" => false
+    ],
     "singers" => [],
     "current" => ["id"=>null,"name"=>"","songArtist"=>""],
     "voting" => [
-      "open"=>false, "endsAt"=>0, "extendCount"=>0,
-      "counts"=>["encore"=>0,"another"=>0,"maybe"=>0],
-      "lastResult"=>null,
-      // track device votes for current singer+song
-      "voters"=>[]  // ["<deviceKey>"=>true]
+      "open" => false, "endsAt" => 0, "extendCount" => 0,
+      "counts" => ["encore"=>0,"another"=>0,"maybe"=>0],
+      "lastResult" => null,
+      "voters" => []   // server-side one-vote-per-device-per-singer+song
     ],
     "winners" => ["encore"=>[], "another"=>[]]
   ];
-  file_put_contents($DATA, json_encode($state, JSON_PRETTY_PRINT));
+  file_put_contents($DATA, json_encode($state, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
   echo json_encode($state);
   exit;
 }
@@ -29,4 +35,5 @@ flock($fp, LOCK_SH);
 $json = stream_get_contents($fp);
 flock($fp, LOCK_UN);
 fclose($fp);
+
 echo $json ?: '{}';
